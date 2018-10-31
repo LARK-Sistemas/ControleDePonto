fd12e0bacb9158c273055b2139df128f89653a99
Rodrigolmg <rodrigo.leitemg@gmail.com>
2018-10-30 23:17:25 -0200

    Commit Inicial

---
+++ ControlePonto/src/main/java/br/com/senai/fatesg/controleponto/entidade/EnumPapelUsuario.java
@@ -0,0 +1,9 @@
+package br.com.senai.fatesg.controleponto.entidade;
+
+public enum EnumPapelUsuario {
+
+   ADMIN,
+   
+   USUARIO;
+   
+}
---
+++ ControlePonto/src/main/java/br/com/senai/fatesg/controleponto/persistencia/ContatoDao.java
@@ -0,0 +1,8 @@
+package br.com.senai.fatesg.controleponto.persistencia;
+
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+import br.com.senai.fatesg.controleponto.entidade.Contato;
+
+public interface ContatoDao extends Persistencia<Contato>{
+
+}
