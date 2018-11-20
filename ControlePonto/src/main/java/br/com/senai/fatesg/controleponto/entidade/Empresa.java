f7cf659c1530e49919e31cb2c98c5000a063a788
kleitonpsld <kleitonpsld@hotmail.com>
2018-11-20 19:09:12 -0200

    Empresa incompleta

--- ControlePonto/src/main/java/br/com/senai/fatesg/controleponto/entidade/Empresa.java
+++ ControlePonto/src/main/java/br/com/senai/fatesg/controleponto/entidade/Empresa.java
@@ -12,141 +12,152 @@ public class Empresa {
 	private long cnpj;
 	private String razaoSocial;
 	private String nomeFantasia;
-	private String endereco;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
 	private String inscricaoEstadual;
 	private String email;
-	private String telefone;
-	private String siteLink;
+	private String fone;
+	private String site;
 	private String redeSocial;
 	private Double porcentagemHorasExtras;
-
 	
-	public Empresa(long id, long cnpj, String razaoSocial, String nomeFantasia, String endereco,
-			String inscricaoEstadual, String email, String telefone, String siteLink, String redeSocial,
-			Double porcentagemHorasExtras) {
+	
+	
+	public Empresa() {
+		super();
+	}
+
+	public Empresa(long id, long cnpj, String razaoSocial, String nomeFantasia, String logradouro, String numero,
+			String complemento, String bairro, String cidade, String estado, String cep, String inscricaoEstadual,
+			String email, String fone, String site, String redeSocial, Double porcentagemHorasExtras) {
 		super();
 		this.id = id;
 		this.cnpj = cnpj;
 		this.razaoSocial = razaoSocial;
 		this.nomeFantasia = nomeFantasia;
-		this.endereco = endereco;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
 		this.inscricaoEstadual = inscricaoEstadual;
 		this.email = email;
-		this.telefone = telefone;
-		this.siteLink = siteLink;
+		this.fone = fone;
+		this.site = site;
 		this.redeSocial = redeSocial;
 		this.porcentagemHorasExtras = porcentagemHorasExtras;
 	}
-
-	public Empresa() {}
-
-
+	
 	public long getId() {
 		return id;
 	}
-
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
 	public long getCnpj() {
 		return cnpj;
 	}
-
-
 	public void setCnpj(long cnpj) {
 		this.cnpj = cnpj;
 	}
-
-
 	public String getRazaoSocial() {
 		return razaoSocial;
 	}
-
-
 	public void setRazaoSocial(String razaoSocial) {
 		this.razaoSocial = razaoSocial;
 	}
-
-
 	public String getNomeFantasia() {
 		return nomeFantasia;
 	}
-
-
 	public void setNomeFantasia(String nomeFantasia) {
 		this.nomeFantasia = nomeFantasia;
 	}
-
-
-	public String getEndereco() {
-		return endereco;
+	public String getLogradouro() {
+		return logradouro;
 	}
-
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
 	}
-
-
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 	public String getInscricaoEstadual() {
 		return inscricaoEstadual;
 	}
-
-
 	public void setInscricaoEstadual(String inscricaoEstadual) {
 		this.inscricaoEstadual = inscricaoEstadual;
 	}
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-	public String getTelefone() {
-		return telefone;
+	public String getFone() {
+		return fone;
 	}
-
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setFone(String fone) {
+		this.fone = fone;
 	}
-
-
-	public String getSiteLink() {
-		return siteLink;
+	public String getSite() {
+		return site;
 	}
-
-
-	public void setSiteLink(String siteLink) {
-		this.siteLink = siteLink;
+	public void setSite(String site) {
+		this.site = site;
 	}
-
-
 	public String getRedeSocial() {
 		return redeSocial;
 	}
-
-
 	public void setRedeSocial(String redeSocial) {
 		this.redeSocial = redeSocial;
 	}
-
-
 	public Double getPorcentagemHorasExtras() {
 		return porcentagemHorasExtras;
 	}
-
-
 	public void setPorcentagemHorasExtras(Double porcentagemHorasExtras) {
 		this.porcentagemHorasExtras = porcentagemHorasExtras;
 	}
+
+	
+	
 }
